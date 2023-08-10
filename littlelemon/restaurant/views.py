dcf620addb7da457bdf22415cf6fd23a68314c11
emzzz56 <eg1712@gmail.com>
2023-08-10 03:51:09 +0300

    Secure APP

--- littlelemon/restaurant/views.py
+++ littlelemon/restaurant/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from rest_framework import generics, viewsets
 from .models import Menu, Booking
 from .serializers import MenuSerializer, BookingSerializer
+from rest_framework.permissions import IsAuthenticated 
 
 
 # Create your views here.
@@ -20,3 +21,4 @@ class SingleMenuItemView(generics.RetrieveUpdateAPIView, generics.DestroyAPIView
 class BookingViewSet(viewsets.ModelViewSet):
     queryset = Booking.objects.all()
     serializer_class = BookingSerializer 
+    permission_classes = [IsAuthenticated]
